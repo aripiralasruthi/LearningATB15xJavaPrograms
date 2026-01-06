cd34be3af262fc8c1e49716a7b9b0c7d3a51362a
aripiralasruthi <aripiralasruthi@gmail.com>
2026-01-06 14:55:35 +0530

    Shortcuts commenting

---
+++ src/ex_02_Java_Basics/Lab007_ShortcutsForCommenting.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics;
+
+public class Lab007_ShortcutsForCommenting {
+    public static void main(String[] args) {
+//        shortcut keys for single line comment is ctrl + /
+        /* shortcut for multi line comment is ctrl + shift + /
+        */
+        System.out.println("\n Shortcut keys for single line comment is ctrl + /");
+        System.out.println("\n /* Shortcut for \n" +
+                "   multi line comment is ctrl + shift + /\n" +
+                "   */");
+    }
+}
