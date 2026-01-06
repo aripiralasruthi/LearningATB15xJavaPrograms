a8091ecdec4733ef673ac891e3f4046ddbfc9117
aripiralasruthi <aripiralasruthi@gmail.com>
2026-01-06 14:24:32 +0530

    comments

---
+++ src/ex_02_Java_Basics/Lab006_MultiLineComments.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basics;
+
+public class Lab006_MultiLineComments {
+    public static void main(String[] args) {
+        // this is the symbol for commenting a line
+        /*
+        this is
+        for multi line
+        commenting
+         */
+        System.out.println("// this is the symbol for commenting a line");
+        System.out.println(" /* \n" +
+                "     this is\n" +
+                "     for multi line\n" +
+                "     commenting\n" +
+                " */");
+    }
+}
