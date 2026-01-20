c19271220880b43747ac2fe6def68278044399f2
aripiralasruthi <aripiralasruthi@gmail.com>
2026-01-20 13:57:28 +0530

    Data Types

---
+++ src/ex_02_Java_Basics/Lab022_DataTypes.java
@@ -0,0 +1,22 @@
+package ex_02_Java_Basics;
+
+public class Lab022_DataTypes {
+    public static void main(String[] args) {
+        byte b = -128;
+        short s = 32767;
+        int i = -128;
+
+        char c = 'A';
+        char c1 = 10;
+        char c2 = '$';
+
+        long l = -23718937298L;
+        long l1 = 8738273323l;
+
+        float f = -3232.4323414f;
+        float F = 878973.3287289F;
+
+        double d1 = 87390.897348937;
+        double d = 87896.78578678;
+    }
+}
