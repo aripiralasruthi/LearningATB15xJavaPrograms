9a83a2e32182b5a217e3b2bd75a621ee611f3f4b
aripiralasruthi <aripiralasruthi@gmail.com>
2026-02-12 12:57:22 +0530

    Variables _ $ Initialising

---
+++ src/ex_02_VariablesAndDatatypes/Lab028_Final_Example.java
@@ -0,0 +1,14 @@
+package ex_02_VariablesAndDatatypes;
+
+public class Lab028_Final_Example {
+    public static void main(String[] args) {
+        // int _ = 10; when initializing - No error message displayed in console
+        // Runtime  - Error message java: underscore not allowed here
+        // System.out.println(_);
+        char $ = 88;
+        System.out.println($);
+        // java: <identifier> expected
+        // long @ = 200;
+        // System.out.println(@);
+    }
+}
