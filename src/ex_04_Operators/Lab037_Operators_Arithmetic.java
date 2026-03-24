57531b7d1d7745b95637366843c4c7ed734482fc
aripiralasruthi <aripiralasruthi@gmail.com>
2026-03-24 12:45:40 +0530

    Operators

---
+++ src/ex_04_Operators/Lab037_Operators_Arithmetic.java
@@ -0,0 +1,22 @@
+package ex_04_Operators;
+
+public class Lab037_Operators_Arithmetic {
+    public static void main(String[] args) {
+        // Arithmetic operators
+        // + (Addition)
+        // - (Subtraction)
+        // * (Multiplication)
+        // / (Division) - Quotient
+        // % (Modulus) - Reminder
+        //int a,b  = 20; // int a = 0; int b = 20;
+        int a = 20;
+        //int b = 7;
+        float b = 7.0f;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        float d = a/b;
+        System.out.println(d);
+        System.out.println(a%b);
+    }
+}
---
+++ src/ex_04_Operators/Lab038_Operators_Unary.java
@@ -0,0 +1,13 @@
+package ex_04_Operators;
+
+public class Lab038_Operators_Unary {
+    public static void main(String[] args) {
+        int a = +10;
+        int b = -110;
+        System.out.println(a+b);
+
+        int c = -1;
+        c = c-1;
+        System.out.println(c);
+    }
+}
